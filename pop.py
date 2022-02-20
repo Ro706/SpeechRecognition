4bb5803f9823d51751eb539ad6e020f998bd68f9
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2022-02-20 23:51:40 +0530

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+filename = "ro.mp3"
+r = sr.Recognizer()
+with sr.AudioFile(filename) as source:
+    # listen for the data (load audio to memory)
+    audio_data = r.record(source)
+    # recognize (convert from speech to text)
+    text = r.recognize_google(audio_data)
+    print(text)
